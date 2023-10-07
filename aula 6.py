d9725117676e48b64aefd35d6917246b66611f27
Vilelaig <146586399+Vilelaig@users.noreply.github.com>
2023-10-07 15:25:57 -0300

    Add files via upload

---
+++ aula 6.py
@@ -0,0 +1,27 @@
+# EXERCICIO 1
+
+# numeros=[1,2,3,4,5]
+# for n in numeros:
+#     print(n) 
+
+# EXERCICIO 2
+# if numeros[2]== 3:
+#      print( 'o 3 está na lista')
+# else:
+#      print(' o 3 nao está na lista')
+      
+
+# EXERCICIO 3
+# numeros=[1,2,3,4,5,6]
+# for n in numeros:
+    # print(n) 
+
+# EXERCICIO 4
+# if numeros[2]== 3:
+#      print( 'o 3 está na lista')
+# else:
+#      print(' o 3 nao está na lista')
+
+# EXERCICIOS 5
+# frutas = ['morango' , 'maracu' , 'manga']
+# print (frutas+numeros)
