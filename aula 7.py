b7dc2d92ac41020d8cbbdf2c61189aad4b885185
Vilelaig <146586399+Vilelaig@users.noreply.github.com>
2023-10-07 17:38:54 -0300

    Add files via upload

---
+++ aula 7.py
@@ -0,0 +1,28 @@
+# contador = 10 
+# while contador > 0:
+#     print(contador)
+#     contador-= 1
+#     print ("CONTADOR")
+
+
+# import random
+# num = random.randint(0,1)
+# chute = int(input("50/50 PARCEIRO BOA SORTE 0 OU 1:"))
+# while chute == num:
+#     print('Parabens vc acerto')
+#     break 
+# else:
+#     print('Você errou!!')
+
+# numero = 11 
+# while numero > 1:
+#           numero -=1 
+# print('FOGO')
+
+insert_number = int(input("Insira um numero par"))
+cont=0
+while cont != insert_number:
+    cont+=2
+    print(cont)
+soma=print(cont)
+print(soma)
