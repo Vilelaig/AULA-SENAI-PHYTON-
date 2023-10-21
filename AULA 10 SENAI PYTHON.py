c3b8b6f3bfbb8389b090357a86a06089c66b6435
Vilelaig <146586399+Vilelaig@users.noreply.github.com>
2023-10-21 17:48:38 -0300

    Add files via upload

---
+++ AULA 10 SENAI PYTHON.py
@@ -0,0 +1,52 @@
+# def soma_list(lista):
+#     soma=0
+#     for numero in lista:
+#         soma += numero 
+#         return print(soma)
+#     soma_list ([1,3,6,6])
+
+#     Exercício 1: Escreva uma função que calcule 
+# a soma dos números de 1 a N, 
+# onde N é um número inteiro dado. 
+
+
+
+# def soma(N):
+#     soma = 0
+#     for i in range(1,N+1):
+#         soma = soma + i
+#         print(soma)
+# soma(3)
+       
+
+
+
+#  Exercício 2: Escreva uma função que 
+# conte quantas vezes uma letra 
+# específica aparece em uma palavra.
+
+# def contar_letra(palavra, letra):
+#     contador = 0
+#     for char in palavra:
+#         if char == letra:
+#             contador += 1
+#             print(f'tem essa quantidade de {letra}:' ,contador )
+
+# achados = contar_letra('abacadabra', 'a')
+
+
+
+# Exercício 3: Escreva uma função que calcule 
+# o fatorial de um número 
+# inteiro não negativo N.
+        
+n = int(input('fat: '))
+resultado = 1 
+count = 1
+
+while count <=n:
+    resultado *= count 
+    count +=1
+    print(resultado)
+
+
