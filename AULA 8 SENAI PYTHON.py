42d7822b00df8ff5a5e55178683030b030902081
Vilelaig <146586399+Vilelaig@users.noreply.github.com>
2023-10-21 11:09:25 -0300

    Add files via upload

---
+++ AULA 8 SENAI PYTHON.py
@@ -0,0 +1,70 @@
+# minha_tupla = (1,2,3,4,5)
+# list = [1,2,3,4]
+
+
+
+# tuple_lista = (1,2,3,5,6)
+# tup,e. append(2)
+# print(tuple[1])
+# print(index)
+
+# tupla = (1,3,978,78)
+# minha_lista = list(tuple)
+# print (x)
+
+# tuple = ('a' , 'b' , 'c')
+# for n in tuple:
+#     print(n)
+
+#     list = (1,3)
+#     x , y = list 
+#     print ('coordenada' , x)
+#     print('coordenada' , y )
+
+#    1 toda funçao é criada 
+# def display(name):
+#     print(' boa noite,' , name, name_2, name_3)
+
+#  a variavel local so pode ser usada dentro da função
+
+    # 2 e depois chamada
+# name = 'julia'
+# name_2 = 'jotape'
+# name_3 = 'pompeu'
+
+# display('julia')  
+
+# EXERCICIOS 
+#  1 -  Crie uma tupla chamada frutas com pelo menos 5 frutas diferentes. Em seguida, acesse e imprima o terceiro elemento da tupla. 
+
+# tuple_lista = ('banana', 'maça', 'pera', 'morango', 'uva' )
+
+# print('a fruta escolhida foi', tuple_lista[2])
+
+#  2 - Crie uma tupla chamada `numeros` com alguns números inteiros. Em seguida, converta essa tupla em uma lista e imprima a lista resultante.
+
+# numeros = ( 1,2,3,4,5,6 )
+# n = list(numeros)
+
+
+# print(' os numeros inteiros chamados são ' , n )
+
+# 3 - Crie uma tupla chamada `meses` com os nomes dos meses do ano até Setembro. Use um loop `for` para imprimir cada mês em uma linha separada.
+
+# meses = ('janeiro', 'fevereiro','março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro')
+# m = list(meses)
+# print(' os meses de pagamentos serão', m)
+
+
+# 4 - Crie uma lista chamada notas com algumas notas de alunos. Em seguida, converta essa lista em uma tupla e imprima a tupla resultante.
+
+# notas = ( 5, 6, 8, 10 )
+# m = tuple(notas)
+# print ('as notas dos alunos são ' , m )
+
+# 5 - Crie uma lista chamada ponto que represente as coordenadas (x, y) de um ponto. Em seguida, desempacote os elementos da lista em duas variáveis separadas (x e y) e imprima os valores.
+
+ponto =  [6000,7000]
+x,y = ponto
+print("coordenada 1" , x)
+print("coordenada 2" , y)
