e686cd3b2b79f1eef63bbcc6606a32a9d6bbbf62
Vilelaig <146586399+Vilelaig@users.noreply.github.com>
2023-11-11 17:43:26 -0300

    AULA 9 - PYTHON.py

--- AULA 9 SENAI PYTHON.py
+++ AULA 9 SENAI PYTHON.py
@@ -1,12 +1,12 @@
-sinonimos_palavras={
-'vacilão':'babacão',
-'pobre':'duro',
-'feio':'horrivel',
-'belo':'lindo',
-}
+# sinonimos_palavras={
+# 'vacilão':'babacão',
+# 'pobre':'duro',
+# 'feio':'horrivel',
+# 'belo':'lindo',
+# }
 
-palavras = input('Digite a palavra: ')
-if palavras in sinonimos_palavras:
-    print(sinonimos_palavras[palavras])
-else:
-    print(f'A palavra {palavras} não existe!')
+# palavras = input('Digite a palavra: ')
+# if palavras in sinonimos_palavras:
+#     print(sinonimos_palavras[palavras])
+# else:
+#     print(f'A palavra {palavras} não existe!')
